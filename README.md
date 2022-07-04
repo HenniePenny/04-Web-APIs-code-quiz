dbbc0164b2f34ebe4b615a8eb5c24be6a40ef3d5
HenniePenny <h.kressner@hotmail.de>
2022-07-04 23:16:10 +0200

    apply some css styles, change questions array

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # 04-Web-APIs-code-quiz
+
+The task is to create a timed quiz on JavaScript fundamentals that stores high scores of the participants. 
+
+## Screenshot
+
+## Acceptance Criteria
+
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+
+## Special Challenges
+- identifying the right or wrong answers in a function
+- subtracting time from timer for wrong answers
+- moving to next question on the click of any answer button in the previous question
+- displaying the "right" or "wrong" confirmation after answering a question
+
+
+## Links
--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,14 @@ const timeEl = document.getElementById("timer");
 const container = document.getElementById("container");
 const startButton = document.getElementById("start-button");
 const rightWrong = document.getElementById("rightorwrong");
+let questionIndex = 0;
+let timeLeft = 75;
+
+// let answerOption1
+// let answerOption2
+// let answerOption3
+// let answerOption4
+
 
 //let timer;
 //let timerCount;
@@ -10,10 +18,11 @@ const rightWrong = document.getElementById("rightorwrong");
 const questions = [
   {
     question: "Which of these is a valid JavaScript comment?",
-    answer1: "//This comment is valid", correct: true,
-    answer2: "<!--This comment is valid-->", correct: false,
-    answer3: "This comment is valid", correct: false,
-    answer4: "?This comment is valid", correct: false,
+    answer1: "//This comment is valid",
+    answer2: "/This comment is valid",
+    answer3: "This comment is valid",
+    answer4: "?This comment is valid",
+    correctAnswer: "//This comment is valid"
     
     /*answers: [
       {text: "// This comment is valid", correct: true},
@@ -24,10 +33,11 @@ const questions = [
   },
   {
     question: "Which one of the following is not used for a variable in Javascript?",
-    answer1: "let", correct: false,
-    answer2: "def", correct: true,
-    answer3: "const", correct: false,
-    answer4: "var", correct: false,
+    answer1: "let",
+    answer2: "def",
+    answer3: "const",
+    answer4: "var",
+    correctAnswer: "def"
     /*answers: [
       {text: "let", correct: false},
       {text: "def", correct: true},
@@ -37,10 +47,11 @@ const questions = [
   },
   {
     question: "Arrays in JavaScript can be used to store...",
-    answer1: "numbers and strings", correct: false,
-    answer2: "other arrays", correct: false,
-    answer3: "booleans", correct: false,
-    answer4: "all of the above", correct: true,
+    answer1: "numbers and strings",
+    answer2: "other arrays",
+    answer3: "booleans",
+    answer4: "all of the above",
+    correctAnswer: "all of the above"
     /*answers: [
       {text: "numbers and strings", correct: false},
       {text: "other arrays", correct: false},
@@ -50,10 +61,11 @@ const questions = [
   },
   {
     question: "Where in the HTML file to we link to the JavaScript file?",
-    answer1: "inside the head tag", correct: false,
-    answer2: "anywhere", correct: false,
-    answer3: "behind the last html tag", correct: false,
-    answer4: "before the closing of the body tag", correct: true,
+    answer1: "inside the head tag",
+    answer2: "anywhere",
+    answer3: "behind the last html tag",
+    answer4: "before the closing of the body tag",
+    correctAnswer: "before the closing of the body tag"
     /*answers: [
       {text: "inside the head tag", correct: false},
       {text: "anywhere", correct: false},
@@ -63,10 +75,11 @@ const questions = [
   },
   {
     question: "How do you create a function in JavaScript?",
-    answer1: "function myFunction()", correct: false,
-    answer2: "function: myFunction()", correct: false,
-    answer3: "function = myFunction()", correct: true,
-    answer4: "function - myFunction()", correct: false,
+    answer1: "function myFunction()",
+    answer2: "function: myFunction()",
+    answer3: "function = myFunction()",
+    answer4: "function - myFunction()",
+    correctAnswer: "function = myFunction()"
     /*answers: [
       {text: "function myFunction()", correct: false},
       {text: "function: myFunction()", correct: false},
@@ -81,60 +94,70 @@ function init() {
   getHighScores();
 }
 
+ 
+
 const startQuiz = function() {
   //timer shows 0 before clicking the start button
-  timerCount = 0;
-  //When I click the start button, set the interval to 75 seconds
-  startTimer();
-  
-  //When I click the start button, empty the container and start filling it with the questions one after another
-  container.innerText = "";
+ timerCount = 0;
 
-  //for each question, creat and element and append the question and answer buttons
-  for (var i=0; i < 1; i++) {
-    const questionDisplay = document.createElement("div");
+ //When I click the start button, set the interval to 75 seconds
+ startTimer();    
 
-    //create the html element to display the question
-    const quizQuestionEl = document.createElement("h3");
-    // if I want to add style, add classes quizQuestionEl.classList.add("question-style")
-    const answerOption1 = document.createElement("button");
-    //add a class for button styling
-    answerOption1.classList.add("btn");
-    const answerOption2 = document.createElement("button");
-    answerOption2.classList.add("btn");
-    const answerOption3 = document.createElement("button");
-    answerOption3.classList.add("btn");
-    const answerOption4 = document.createElement("button");
-    answerOption4.classList.add("btn");
-
-    //assign content to created elements
-    quizQuestionEl.innerHTML = questions[i].question;
-    answerOption1.innerHTML = questions[i].answer1;
-    answerOption2.innerHTML = questions[i].answer2;
-    answerOption3.innerHTML = questions[i].answer3;
-    answerOption4.innerHTML = questions[i].answer4;
-
-    //appending the above-created elements to show on the page
-    container.appendChild(questionDisplay);
-    container.appendChild(quizQuestionEl);
-    container.appendChild(answerOption1);
-    container.appendChild(answerOption2);
-    container.appendChild(answerOption3);
-    container.appendChild(answerOption4);
-  }
+ 
   
   //Clicking on start button displays question 1 only (not all of the questions at the same time)
-  startButton.addEventListener("click", function(event) {
-  // Stops event from bubbling up and new window opening
-  event.stopPropagation();
-  })
+  //startButton.addEventListener("click", function(event) {
+  //Stops event from bubbling up and new window opening
+  //event.stopPropagation(event);
+  //})
+  setNextQuestion();
+}
+
+function setNextQuestion() {
+   //When I click the start button, empty the container and start filling it with the questions one after another
+   container.innerText = "";
+
+   //for each question, create and element and append the question and answer buttons
+   //for (var i=0; i < 1; i++) {
+     const questionDisplay = document.createElement("div");
+ 
+     //create the html element to display the question
+     const quizQuestionEl = document.createElement("h3");
+     // if I want to add style, add classes quizQuestionEl.classList.add("question-style")
+     const answerOption1 = document.createElement("button");
+     //add a class for button styling
+     answerOption1.classList.add("btn");
+     //add event listener for button
+     answerOption1.addEventListener("click", evaluateAnswer);
+     const answerOption2 = document.createElement("button");
+     answerOption2.classList.add("btn");
+     answerOption2.addEventListener("click", evaluateAnswer);
+     const answerOption3 = document.createElement("button");
+     answerOption3.classList.add("btn");
+     answerOption3.addEventListener("click", evaluateAnswer);
+     const answerOption4 = document.createElement("button");
+     answerOption4.classList.add("btn");
+     answerOption4.addEventListener("click", evaluateAnswer);
+     
+     //assign content to created elements
+     quizQuestionEl.innerHTML = questions[questionIndex].question;
+     answerOption1.innerHTML = questions[questionIndex].answer1;
+     answerOption2.innerHTML = questions[questionIndex].answer2;
+     answerOption3.innerHTML = questions[questionIndex].answer3;
+     answerOption4.innerHTML = questions[questionIndex].answer4;
+ 
+     //appending the above-created elements to show on the page
+     container.appendChild(questionDisplay);
+     container.appendChild(quizQuestionEl);
+     container.appendChild(answerOption1);
+     container.appendChild(answerOption2);
+     container.appendChild(answerOption3);
+     container.appendChild(answerOption4);
 }
 
 //start Timer function is called when Start button is clicked
 function startTimer() {
   //set timer
-  let timeLeft = 75;
-
   let timer = setInterval(function() {
     //as long as timeLeft is greater than 1
     if (timeLeft > 1) {
@@ -145,17 +168,55 @@ function startTimer() {
     } else {
       timeEl.textContent = "";
       clearInterval(timer);
-      displayScore();
+      displayScore(); //TODO
     }
   }, 1000)
 }
+function quizEnd() {
+  //TODO
+}
 
+
+function evaluateAnswer() {
+  if(this.value !== questions[questionIndex].correctAnswer) {
+    timeLeft -= 10;
+    if(timeLeft < 0) {
+      timeLeft = 0
+    }
+    timeEl.textContent = timeLeft; 
+    rightWrong.textContent = "Wrong!";
+  }
+    else {
+      rightWrong.textContent = "Correct!";
+    }
+
+  setTimeout(function() {
+    rightWrong.setAttribute("class", hide);
+  }, 1000); 
+  questionIndex++;
+  if(questionIndex === questions.length) {
+    quizEnd();
+  } else {
+    setNextQuestion();
+  }
+}
 
 //after the click of any answer button, move to next question
+/*function setNextQuestion() {
+  if (answerOption1 === true || answerOption2 === true || answerOption3 === true || answerOption4 === true) {
+    rightorWrong.textContent = "Correct!"
+    //and move to next question
+  } else {
+    rightorWrong.textContent = "Wrong!"
+    //and move to next question
+  }
+  //if I click on a true answer, show true and move to next question
+  /*if ("button" === true) {
 
-function setNextQuestion() {
-  
-}
+  }
+  //if I click on false answer, show false, deduct time and move to next question
+ // container.innerText = questions.question
+}*/
 
 
 
@@ -178,24 +239,6 @@ function getHighScores () {
   var storedHighScores = localStorage.getItem()
 }
 
-/*let count = 0;
-let countDownMS = 75000;
-
-function countdown() {
-  //timer counts down from 75s
-  var timeLeft = 75;
-  
-  //use the setInterval() method to call a function to be executed every 75000 milliseconds
-  var timeInterval = setInterval(function() {
-    if (timeLeft > 1) {
-        timeLeft--;
-    } else if {
-        
-    }
-  }
-    
-  )
-}*/
 
 
 //add event listeners
